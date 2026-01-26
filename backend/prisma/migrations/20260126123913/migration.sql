bb238146011914a35b94101cc9ca60e6269e5595
The-Yearly <theyearlysclub@gmail.com>
2026-01-26 18:13:29 +0530

    Updates Package.json

---
+++ backend/prisma/migrations/20260126123913/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `farmerTest` on the `farmer` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "farmer" DROP COLUMN "farmerTest";
